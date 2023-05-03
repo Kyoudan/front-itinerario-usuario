e90d5fc273a5a82d7f747126c9aa37bce9c8b271
Kyoudan <fogoste@gmail.com>
2023-05-03 11:52:36 -0300

    add: pagina para ativação de conta

---
+++ src/pages/ActivateAccount/index.tsx
@@ -0,0 +1,116 @@
+import { useNavigate, useParams } from "react-router-dom";
+import * as S from "./style";
+import { BsCheck, BsXLg } from "react-icons/bs";
+import { useAppThemeContext } from "../../contexts/ThemeContext";
+import { Button, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { api } from "../../api/api";
+
+export const ActivateAccount = () => {
+  const { uuid } = useParams();
+  const { themeName } = useAppThemeContext();
+  const [timeLeft, setTimeLeft] = useState(5);
+  const [state, setState] = useState<"error" | "correct" | "">("");
+  const [message, setMessage] = useState("");
+  const navigate = useNavigate();
+
+  const handleActivateAccount = async () => {
+    try {
+      console.log("Chamado!!");
+      const result = await api.get(`/activateAccount/?token=${uuid}`);
+      if (state == "" && message == "") {
+        setState("correct");
+        setMessage(result.data.message);
+      }
+    } catch (err: any) {
+      setState("error");
+      setMessage(err.response.data.message);
+    }
+  };
+
+  useEffect(() => {
+    if (state === "correct") {
+      if (timeLeft === 0) {
+        navigate("/");
+        return;
+      }
+
+      const timerId = setInterval(() => {
+        setTimeLeft((prevTimeLeft) => prevTimeLeft - 1);
+      }, 1000);
+
+      return () => clearInterval(timerId);
+    }
+  }, [timeLeft]);
+
+  return (
+    <>
+      {message ? (
+        <S.styledDiv theme={themeName}>
+          {state === "correct" ? (
+            <div className="circle-wrapper">
+              <div className="success circle"></div>
+              <div className="icon">
+                <BsCheck />
+              </div>
+            </div>
+          ) : (
+            <div className="circle-wrapper">
+              <div className="error circle"></div>
+              <div className="icon">
+                <BsXLg />
+              </div>
+            </div>
+          )}
+
+          <Typography
+            sx={{
+              fontFamily: "Montserrat",
+              fontSize: "1.5em",
+              color: themeName === "dark" ? "#fff" : "#000",
+              textAlign: "center",
+            }}
+            className="entryTypography"
+          >
+            {message}
+          </Typography>
+          {state === "correct" && (
+            <Typography
+              sx={{
+                fontFamily: "Montserrat",
+                fontSize: "0.8em",
+                color: themeName === "dark" ? "#fff" : "#000",
+                textAlign: "center",
+              }}
+              className="entryTypography"
+            >
+              Redirecionando você para pagina de login em: {timeLeft}s
+            </Typography>
+          )}
+        </S.styledDiv>
+      ) : (
+        <>
+          <S.styledDiv theme={themeName}>
+            <Button
+              sx={{
+                height: "40px",
+                fontSize: "1em",
+                fontFamily: "Montserrat",
+                color: themeName === "dark" ? "#fff" : "#1c1c1c",
+                border:
+                  themeName === "dark" ? "1px solid #fff" : "1px solid #1c1c1c",
+                marginTop: "10px",
+                backgroundColor:
+                  themeName === "dark" ? "#a60303" : "transparent",
+              }}
+              variant="text"
+              onClick={handleActivateAccount}
+            >
+              Ativar
+            </Button>
+          </S.styledDiv>
+        </>
+      )}
+    </>
+  );
+};
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "../pages/Home";
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
+import { ActivateAccount } from "../pages/ActivateAccount";
 
 export const RoutesComponent = () => {
   return (
@@ -10,6 +11,7 @@ export const RoutesComponent = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/register" element={<Register />}></Route>
+        <Route path="/activateAccount/:uuid" element={<ActivateAccount />}></Route>
       </Routes>
     </BrowserRouter>
   );
