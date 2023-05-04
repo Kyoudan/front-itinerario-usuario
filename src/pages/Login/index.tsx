886bb603ce74221ba0d2fff924e233975f45e1e3
Martins-pedro23 <martinsdeoliveira.pedro2@gmail.com>
2023-05-04 20:11:52 -0300

    add: pagina de login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,3 +1,20 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Button from "@mui/material/Button";
+
+
 export const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <Card sx={{ maxWidth: 275 }}>
+        <CardContent>
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+          <TextField id="outlined-basic" label="Senha" variant="outlined" />
+        </CardContent>
+      </Card>
+    </div>
+  );
 };
